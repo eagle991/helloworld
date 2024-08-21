dab0af697d467a51a30e80523603595537bb5488
Kirill Orlov <eagle991@mail.ru>
2024-08-21 21:07:33 +0300

    added variable tasks 1,2,3,4,5,6,7,8

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Привет Мир!");
         //задача 1
         var dog = 8.0;
         System.out.println ("переменная dog = " +dog);
