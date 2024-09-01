f6af9f887c648935836875c9f2ba9ad23b7ff3f0
Kirill Orlov <eagle991@mail.ru>
2024-09-01 14:38:35 +0300

    conditional operator lesson 2

--- src/Main.java
+++ src/Main.java
@@ -37,18 +37,17 @@ public class Main {
         {
 
             int deliveryDistance = 95;
-            if (deliveryDistance <= 20){
-                System.out.println("Потребуется дней"+1 );
-            } else if (deliveryDistance >20 && deliveryDistance <=60){
-                System.out.println("Потребуется дней"+2 );
-            }else if (deliveryDistance >60 && deliveryDistance <=100){
-                System.out.println("Потребуется дней"+3 );
-            }else{
+            if (deliveryDistance <= 20) {
+                System.out.println("Потребуется дней" + 1);
+            } else if (deliveryDistance > 20 && deliveryDistance <= 60) {
+                System.out.println("Потребуется дней" + 2);
+            } else if (deliveryDistance > 60 && deliveryDistance <= 100) {
+                System.out.println("Потребуется дней" + 3);
+            } else {
                 System.out.println("нет доставки");
             }
-
+        }
 //задача 5
-
             {
                 int mountNumber = 12;
                 switch (mountNumber) {
