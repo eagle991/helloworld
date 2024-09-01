00e361c0b9a87575d1a7f29d1f2e76c360d39e0f
Kirill Orlov <eagle991@mail.ru>
2024-09-01 23:36:51 +0300

    edited 1,2,5

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,22 @@ public class Main {
     public static void main(String[] args) {
 //задача 1
         {
-            int os = 0;
-            if (os == 0) {
-                System.out.println("Установите версию приложения для ios по ссылке");
-            }else if (os ==1){
-                System.out.println("Установите версию приложения для Android по ссылке");
+            int clientOS = 0;
+            if (clientOS == 0) {
+                operationSystem = "iOS";
+                System.out.println("Установите версию приложения для"+operationSystem+ "по ссылке");
+            }else if (clientOS ==1){
+                operationSystem = "Android";
+                System.out.println("Установите версию приложения для" +operationSystem+" по ссылке");
             }else{
                 System.out.println("Неверное значение");
             }
         }
 //задача 2
         {
-            int ios = 0;
+            int os = 0;
             int clientDeviceYear = 2015;
-            if (ios == 0 && clientDeviceYear < 2015) {
+            if (os == 0 && clientDeviceYear < 2015) {
                 System.out.println("Установите облегченную версию приложения для ios по ссылке");
             }else if (os == 0 && clientDeviceYear >= 2015){
             System.out.println("Установите версию приложения для ios по ссылке");
@@ -49,8 +51,8 @@ public class Main {
         }
 //задача 5
             {
-                int mountNumber = 12;
-                switch (mountNumber) {
+                int monthNumber = 12;
+                switch (monthNumber) {
                     case 12:
                     case 1:
                     case 2:
