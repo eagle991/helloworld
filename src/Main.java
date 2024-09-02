34384c9634a597d22aeb6ac6aa20107fb2dfdf87
Kirill Orlov <eagle991@mail.ru>
2024-09-02 20:28:51 +0300

    edited 2

--- src/Main.java
+++ src/Main.java
@@ -17,12 +17,18 @@ public class Main {
         {
             int os = 0;
             int clientDeviceYear = 2015;
-            if (os == 0 && clientDeviceYear < 2015) {
-                System.out.println("Установите облегченную версию приложения для ios по ссылке");
-            }else if (os == 0 && clientDeviceYear >= 2015){
-            System.out.println("Установите версию приложения для ios по ссылке");
-        }else {
-            System.out.println("Установите версию приложения для Android по ссылке");
+            if (os == 0) {
+                if (clientDeviceYear < 2015) {
+                    System.out.println("Установите облегченную версию приложения для ios по ссылке");
+            }else  {
+                    System.out.println("Установите версию приложения для ios по ссылке");
+                }
+            }else if (os ==1){
+                if(clientDeviceYear <2015) {
+                    System.out.println("Установите облегченную версию приложения для Android по ссылке");
+                }else {
+                    System.out.println("Установите версию приложения для Android по ссылке");
+                }
         }
         }
 //задача 3
