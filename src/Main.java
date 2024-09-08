9db69bdff34ed3794e89451ce56b22b9c839f88e
Kirill Orlov <eagle991@mail.ru>
2024-09-09 00:14:31 +0300

    Cycle lesson 1

--- src/Main.java
+++ src/Main.java
@@ -1,103 +1,59 @@
 public class Main {
     public static void main(String[] args) {
-//задача 1
-        {
-            int age = 19;
-            if (age >= 18) {
-                System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
-            }
-        }
-        {
-            int age = 17;
-            if (age >= 18) {
-                System.out.println("Если возраст человека равен " + age + ", то он совершеннолетний");
-            } else {
-                System.out.println("Если возраст человека равен " + age + ", он не достиг совершеннолетия, нужно немного подождать");
-            }
-        }
-        //Задача 2
-        {
-            int temp = 4;
-            if (temp < 5) {
-                System.out.println("На улице холодно, нужно надеть шапку "+temp+" градуса");
-            }else {
-                System.out.println("Сегодня тепло, можно идти без шапки "+temp+" градусов");
-            }
-        }
-        //задача 3
-        {
-            int speed = 59;
-            if (speed >= 60) {
-                System.out.println("Если скорость " + speed + ", то придется заплатить штраф");
-            } else {
-                System.out.println("Если скорость " + speed + ", можно ездить спокойно");
-            }
-        }
-        {
-            int speed = 61;
-            if (speed >= 60) {
-                System.out.println("Если скорость " + speed + ", то придется заплатить штраф");
-            } else {
-                System.out.println("Если скорость " + speed + ", можно ездить спокойно");
-            }
-        }
-        //задача 4
-        {
-            int age = 55;
-            if (age >= 2 && age <=6){
-                System.out.println("Если возраст человека равен"+age+", то ему нужно ходить в детский сад");
-            } else if (age >= 7 && age <=17) {
-                System.out.println("Если возраст человека равен" + age + ", то ему нужно ходить в школу");
-            } else if (age >= 18 && age <=24) {
-                System.out.println("Если возраст человека равен" + age + ", то ему нужно ходить в университет");
-            } else if (age > 24 ) {
-                System.out.println("Если возраст человека равен" + age + ", то ему нужно ходить на работу");
-            }else {
-                System.out.println("Если возраст человека равен " + age + ", то ему нужно кудато ходить");
-            }
-        }
-        //задача 5
-        {
-            int age = 7;
-            if (age < 5) {
-                System.out.println("Если возраст человека равен " + age + ", то ему можно кататься");
-            }
-            if(age >= 5 && age <14) {
-                System.out.println("Если возраст человека равен " + age + ", то ему нельзя кататься в сопровождении");
-            } else if(age > 14) {
-                System.out.println("Если возраст человека равен " + age + ", то ему можно кататься без сопровождения");
-            }else {
-                System.out.println("Если возраст человека равен " + age + ", то ему можно что-то");
-            }
+//Задача №1
+        for (int i =1; i <=10; i++) {
+            System.out.print(i + " ");
         }
 
-        //задача 6
-        {
-            int places = 102;
-            int sitPlace = 60;
-            int people = 110;
-            if (place < sitPlace) {
-                {
-                    System.out.println("в вагоне есть сидячие места");
-                }
-                if (people >= sitPlace && people < places) {
-                    System.out.println("в вагоне есть стоячие места");
-                }
-                if (people >= places) {
-                    System.out.println("в вагоне нет мест");
-                }
+//Задача №2
+        for (int i =10; i =>1; i--) {
+            System.out.print(i + " ");
+        }
+
+//Задача №3
+        for (int i =0; i < 17; i++) {
+            if (i % 2 == 0){
+                System.out.print(i + " ");
             }
-            //задача 7
-            {
-                int one = 10;
-                int two = 5;
-                int three = 30;
-                if (one >= two && one >= three) {
-                    System.out.println("Самое большое число - " +one);
-                } else {
-                    if(two >= three) {
-                        System.out.println("Самое большое число - " +two);
-                    }else{
-                        System.out.println("Самое большое число - " +three);
-                    }
+
+//Задача №4
+            for (int i =10; i =>-10; i--) {
+                System.out.print(i + " ");
+            }
+
+//Задача №5
+            for (int year =1904; year <=2096; year+=4) {
+                System.out.print(year + "год высокосный");
+            }
+
+//Задача №6
+            for (int i =7; i <=98; i+=7) {
+                System.out.print(i + " ");
+            }
+
+//Задача №7
+            for (int i =1; i <=512; i*=2) {
+                System.out.print(i + " ");
+            }
+
+//Задача №8
+            int amount = 29_000;
+            int sum = 0;
+            for (int month =1; month <=12; month*=++) {
+                sum+=amount;
+                System.out.print("Месяц"+month + "сумма накопления ровна"+sum+"руб");
+            }
+
+//Задача №9
+            int sum = 0;
+            double percent = 1d/100;
+            for (int month =1; month <=12; month*=++) {
+                sum+=amount;
+                sum=(int) (sum*(1+percent));
+                System.out.print("Месяц"+month + "сумма накопления ровна"+sum+"руб");
+            }
+//Задача №10
+            for (int multiplier =1; multiplier <=10; multiplier++) {
+                System.out.printf("%d x %d = %d%n",2,multiplier);
+            }
                 }
