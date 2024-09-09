05a07f5cc6a53275fd0ed58f85760d983b80d42f
Kirill Orlov <eagle991@mail.ru>
2024-09-09 22:48:01 +0300

    Cycle lesson 1 corrections 2,3,5,8,9,10

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,12 @@ public class Main {
         }
 
 //Задача №2
-        for (int i =10; i =>1; i--) {
+        for (int i =10; i >=1; i--) {
             System.out.print(i + " ");
         }
 
 //Задача №3
-        for (int i =0; i < 17; i++) {
+        for (int i =0; i <= 17; i++) {
             if (i % 2 == 0){
                 System.out.print(i + " ");
             }
@@ -23,7 +23,7 @@ public class Main {
 
 //Задача №5
             for (int year =1904; year <=2096; year+=4) {
-                System.out.print(year + "год высокосный");
+                System.out.print(year + "год является високосным");
             }
 
 //Задача №6
@@ -39,21 +39,21 @@ public class Main {
 //Задача №8
             int amount = 29_000;
             int sum = 0;
-            for (int month =1; month <=12; month*=++) {
-                sum+=amount;
-                System.out.print("Месяц"+month + "сумма накопления ровна"+sum+"руб");
+            for (int month = 1; month <= 12; month ++) {
+                sum += amount;
+                System.out.println("Месяц " + month + " сумма накопления ровна " + sum + " руб");
             }
 
 //Задача №9
+            int amount = 29_000;
             int sum = 0;
             double percent = 1d/100;
-            for (int month =1; month <=12; month*=++) {
+            for (int month = 1; month <= 12; month ++) {
                 sum+=amount;
                 sum=(int) (sum*(1+percent));
-                System.out.print("Месяц"+month + "сумма накопления ровна"+sum+"руб");
+                System.out.println("Месяц"+month + "сумма накопления ровна"+sum+"руб");
             }
 //Задача №10
-            for (int multiplier =1; multiplier <=10; multiplier++) {
-                System.out.printf("%d x %d = %d%n",2,multiplier);
+            for (int multiplier = 1; multiplier <= 10; multiplier++) {
+                System.out.printf("%d x %d = %d%n", 2, multiplier, 2 *multiplier );
             }
-                }
