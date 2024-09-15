dc64cef053809dba36a156ae2099a8b23493202a
Kirill Orlov <eagle991@mail.ru>
2024-09-15 23:59:44 +0300

    Arrays lesson 1

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,45 @@
 public class Main {
     public static void main(String[] args) {
-//Задача №1
-        for (int i =1; i <=10; i++) {
-            System.out.print(i + " ");
+//задача 1
+        int[] ints = new int[12];
+        System.out.println(Arrays.toString(ints));
+
+        double[] doubles = new double[3];
+        doubles[0] =1.57;
+        doubles[1] =7.654;
+        doubles[2] =9.986;
+
+        boolean[] booleans = new boolean[2];
+        booleans[0] = false;
+        booleans[1] = false;
+        System.out.println(Arrays.toString(booleans));
+        String[] strings ={"125","135","145"};
+        System.out.println(Arrays.toString(strings));
+    }
+
+    //задача 2
+    int[] ints1 = {1, 3, 4, 56,7};
+for(int i = 0; i < ints1.length; i++) {
+        System.out.print(ints1[i]);
+        if (i < ints1.length - 1) {
+            System.out.print(",");
         }
 
-//Задача №2
-        for (int i =10; i >=1; i--) {
-            System.out.print(i + " ");
+//задача 3
+        for(int i = ints1.length-1; i >= 0; i--) {
+            System.out.print(ints1[i]);
+            if (i != 0) {
+                System.out.print(",");
+            }
+
+//задача 4
+            int[] ints2 = {1, 3, 4, 56,7};
+            for(int i = 0; i < ints2.length; i++) {
+                if (ints2[i] % 2 !=0) {
+                    ints2[i]++;
+                }
+            }
+            System.out.println(Arrays.toString(ints2));
         }
-
-//Задача №3
-        for (int i =0; i <= 17; i++) {
-            if (i % 2 == 0){
-                System.out.print(i + " ");
-            }
-
-//Задача №4
-            for (int i =10; i =>-10; i--) {
-                System.out.print(i + " ");
-            }
-
-//Задача №5
-            for (int year =1904; year <=2096; year+=4) {
-                System.out.print(year + "год является високосным");
-            }
-
-//Задача №6
-            for (int i =7; i <=98; i+=7) {
-                System.out.print(i + " ");
-            }
-
-//Задача №7
-            for (int i =1; i <=512; i*=2) {
-                System.out.print(i + " ");
-            }
-
-//Задача №8
-            int amount = 29_000;
-            int sum = 0;
-            for (int month = 1; month <= 12; month ++) {
-                sum += amount;
-                System.out.println("Месяц " + month + " сумма накопления ровна " + sum + " руб");
-            }
-
-//Задача №9
-            int amount = 29_000;
-            int sum = 0;
-            double percent = 1d/100;
-            for (int month = 1; month <= 12; month ++) {
-                sum+=amount;
-                sum=(int) (sum*(1+percent));
-                System.out.println("Месяц"+month + "сумма накопления ровна"+sum+"руб");
-            }
-//Задача №10
-            for (int multiplier = 1; multiplier <= 10; multiplier++) {
-                System.out.printf("%d x %d = %d%n", 2, multiplier, 2 *multiplier );
-            }
+    }
+}
