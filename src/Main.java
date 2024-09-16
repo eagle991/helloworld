4000cc4dd8f6199943059ae55b4226f1635ff8c0
Kirill Orlov <eagle991@mail.ru>
2024-09-16 23:27:53 +0300

    Arrays lesson 1 corrections 2,3

--- src/Main.java
+++ src/Main.java
@@ -1,43 +1,45 @@
+import java.util.Arrays;
+
+
 public class Main {
     public static void main(String[] args) {
-//задача 1
+        //задача 1
         int[] ints = new int[12];
         System.out.println(Arrays.toString(ints));
 
         double[] doubles = new double[3];
-        doubles[0] =1.57;
-        doubles[1] =7.654;
-        doubles[2] =9.986;
+        doubles[0] = 1.57;
+        doubles[1] = 7.654;
+        doubles[2] = 9.986;
 
         boolean[] booleans = new boolean[2];
         booleans[0] = false;
         booleans[1] = false;
         System.out.println(Arrays.toString(booleans));
-        String[] strings ={"125","135","145"};
+
+        String[] strings = {"125", "135", "145"};
         System.out.println(Arrays.toString(strings));
-    }
-
-    //задача 2
-    int[] ints1 = {1, 3, 4, 56,7};
-for(int i = 0; i < ints1.length; i++) {
-        System.out.print(ints1[i]);
-        if (i < ints1.length - 1) {
-            System.out.print(",");
+        // задача 2
+        int[] ints1 = {1, 3, 4, 56, 7};
+        for (int i = 0; i < ints1.length; i++) {
+            System.out.print(ints1[i]);
+            if (i < ints1.length - 1) {
+                System.out.print(", ");
+            }
         }
-
-//задача 3
+        //Задача 3
+        System.out.println( );
         for(int i = ints1.length-1; i >= 0; i--) {
             System.out.print(ints1[i]);
             if (i != 0) {
                 System.out.print(",");
             }
-
-//задача 4
-            int[] ints2 = {1, 3, 4, 56,7};
-            for(int i = 0; i < ints2.length; i++) {
-                if (ints2[i] % 2 !=0) {
-                    ints2[i]++;
-                }
+        }
+        //Задача 4
+        int[] ints2 = {1, 3, 4, 56,7};
+        for(int i = 0; i < ints2.length; i++) {
+            if (ints2[i] % 2 !=0) {
+                ints2[i]++;
             }
             System.out.println(Arrays.toString(ints2));
         }
