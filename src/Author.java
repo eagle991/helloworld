48c91680a55f24b4099eefce37a1869383d76aa5
Kirill Orlov <eagle991@mail.ru>
2024-10-01 02:28:59 +0300

    object method correction

--- src/Author.java
+++ src/Author.java
@@ -1,39 +1,25 @@
-import java.unil.Objects;
-
 public class Author {
-    private String firstName;
-    private String lastName;
+    private  String firstName;
+    private  String lastName;
+
     public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
     }
-    public String getFirstName {
+
+    public String getFirstName() {
         return firstName;
     }
-    public String getFirstName {
+
+    public String getLastName() {
         return lastName;
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) {
-            return true;
-        }
-        if (o == null || getClass() != o.getClass()) {
-            return false;
-        }
-        Author author = (Author) o;
-        return Object.equals(firstName, author.firstName) && Object.equals(lastName, author.lastName);
-    }
-
-    @Override
-    public int hashCode() {
-        return Object.hash(firstName, lastName);
-    }
-
     public String toString() {
-        return "Author{" +
-                "firstName='" + firstName + '\'' +
-                ", lastName'" + lastName + '\'' +
-                '+';
+        return "Автор{" +
+                "Имя='" + firstName + '\'' +
+                ", Фамилия='" + lastName + '\'' +
+                '}';
     }
-
 }
--- src/Book.java
+++ src/Book.java
@@ -1,27 +1,74 @@
+import java.util.Objects;
 
 public class Book {
+    //характеристики
     private String name;
     private int year;
     private Author author;
-
-    public Book(){
-
-    }
+    //конструкторы
     public Book(String name, int year, Author author) {
-        this(name,year);
+        this.name = name;
+        this.year = year;
         this.author = author;
     }
+
     public Book(String name, int year) {
         this.name = name;
         this.year = year;
     }
-    public Book(Author author){
+
+    public Book(Author author) {
         this.author = author;
     }
+
+    public Book() {
+
+    }
+    //методы
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
+
     public String getName() {
         return name;
     }
-    public void setYear(int year) {
-        this.year = year;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return year == book.year && Objects.equals(name, book.name) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, year, author);
+    }
+
+    @Override
+    public String toString() {
+        return "Книга{" +
+                "Имя='" + name + '\'' +
+                ", год=" + year +
+                ", автор=" + author +
+                '}';
     }
 }
