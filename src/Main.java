43e087de21b98cc3dded6f3d27428878e77a4954
Kirill Orlov <eagle991@mail.ru>
2024-10-21 22:49:33 +0300

    Memory in Java

--- src/Main.java
+++ src/Main.java
@@ -1,76 +1,76 @@
-import jdk.nio.mapmode.ExtendedMapMode;
-
-import java.util.Random;
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 public class main {
-    private final static Random RANDOM = new Random();
-
-    private final static String[] NAMES = {"Андрей","Иван","Михаил","Сергей","Евгений","Александр"};
-    private final static String[] SURNAMES = {"Андреев","Иванов","Михаилов","Сергеев","Евгеньев","Александров"};
-    private final static String[] PUTRONYMIC_NAMES = {"Андревич","Иванович","Михаилович","Сергевич","Евгеньевич","Александрович"};
-
-    private final static Employee[] EMPLOYEES = new Employee[10];
-
-    private static void initEmployee(){
-        for (int i = 0 ; i < EMPLOYEES.length; i++){
-            String fullname = SURNAMES[RANDOM.nextInt(0, SURNAMES.length)]+" "+
-                    NAMES[RANDOM.nextInt(0, NAMES.length)]+" "+
-                    PUTRONYMIC_NAMES[RANDOM.nextInt(0, PUTRONYMIC_NAMES.length)];
-            EMPLOYEES[i] = new Employee(fullname, RANDOM.nextInt(1,6), RANDOM.nextInt(50_000,100_000));
-
-        }
-
+    public static void main(String[] args) {
+        //5 Задача
+        int value = 33;
+        System.out.println(value);
+        changeValue(value);
+        System.out.println(value);
     }
-    public static void main(String[] args){
-        initEmployee();
-        print();
-        System.out.println("Сумма ЗП сотрудников: " + calculateSumOfSalaries());
-        System.out.println("Сотрудника с минимальной ЗП: " + findEmployeeWithMineSalary());
-        System.out.println("Сотрудника с максимальной ЗП: " + findEmployeeWithMaxSalary());
-        System.out.println("Сотрудника с средней ЗП: " + calculateAvarageOfSalaries());
-        printFullNames();
+    private static void changeValue(int temp) {
+        temp = 22;
+        System.out.println("changeValue " + temp);
     }
-
-    private static void print(){
-        for(Employee employee: EMPLOYEES){
-            System.out.println(employee);
-        }
+    //------------------------------------------------------------
+    //6 Задача
+    public static void main(String[] args) {
+        Integer value = 44;
+        System.out.println(value);
+        changeValue(value);
+        System.out.println(value);
     }
-
-    private static int calculateSumOfSalaries(){
-        int sum = 0;
-        for(Employee employee: EMPLOYEES){
-            sum+=employee.getSalary();
-        }
-        return sum;
+    private static void changeValue(Integer temp) {
+        temp = 22;
+        System.out.println("changeValue " + temp);
     }
-
-    private static Employee findEmployeeWithMineSalary(){
-        Employee employeeWithMineSalary = null;
-        for(Employee employee: EMPLOYEES){
-            if(employeeWithMineSalary == null || employee.getSalary() < employeeWithMineSalary.getSalary()){
-                employeeWithMineSalary = employee;
-            }
-        }
-        return employeeWithMineSalary;
+    //------------------------------------------------------------
+    //7 Задача
+    public static void main(String[] args) {
+        Integer[] value = {1,2};
+        System.out.println(Arrays.toString(value));
+        changeValue(value);
+        System.out.println(Arrays.toString(value));
     }
-    private static Employee findEmployeeWithMaxSalary(){
-        Employee employeeWithMaxSalary = null;
-        for(Employee employee: EMPLOYEES){
-            if(employeeWithMaxSalary == null || employee.getSalary() > employeeWithMaxSalary.getSalary()){
-                employeeWithMaxSalary = employee;
-            }
-        }
-        return employeeWithMaxSalary;
+    private static void changeValue(Integer[] temp) {
+        temp = new Integer[]{5,5,6};
+        System.out.println("changeValue " +Arrays.toString(temp));
     }
-    private static double calculateAvarageOfSalaries(){
-        return (double) calculateSumOfSalaries() / EMPLOYEES.length;
+    //------------------------------------------------------------
+    //8 Задача
+    public static void main(String[] args) {
+        Integer[] value = {1,2};
+        System.out.println(Arrays.toString(value));
+        changeValue(value);
+        System.out.println(Arrays.toString(value));
     }
-
-    private static void printFullNames(){
-        for(Employee employee : EMPLOYEES) {
-            System.out.println(employee.getFullName());
-        }
+    private static void changeValue(Integer[] temp) {
+        temp[0] = 99;
+        System.out.println("changeValue " +Arrays.toString(temp));
     }
-
+    //------------------------------------------------------------
+    //9 Задача
+    public static void main(String[] args) {
+        Person person = new Person("Trubetskoy", "Lyapis");
+        System.out.println(person);
+        changeValue(person);
+        System.out.println(person);
+    }
+    private static void changeValue(Person temp) {
+        temp = new Person("Lagutenko", "Ilya");
+        System.out.println("changeValue " + temp);
+    }
+    //------------------------------------------------------------
+    //10 Задача
+    public static void main(String[] args) {
+        Person person = new Person("Trubetskoy", "Lyapis");
+        System.out.println(person);
+        changeValue(person);
+        System.out.println(person);
+    }
+    private static void changeValue(Person person) {
+        person = new Person("Lagutenko", "Ilya");
+        System.out.println("changeValue " + person);
+    }
 }
